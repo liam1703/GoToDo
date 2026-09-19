ad8cb7db9121c5c8b8858a3e9f45cc94b0fdfcb2
Liam Bradbury <55163640+liam1703@users.noreply.github.com>
2026-09-19 16:24:47 +0100

    Add a delete function

--- todo.go
+++ todo.go
@@ -1,6 +1,8 @@
 package main
 
-impoert (
+import (
+	"errors"
+	"slices"
 	"time"
 )
 
@@ -9,4 +11,31 @@ type Todo struct{
 	Completed bool
 	CreatedAt time.Time
 	CompletedAt *time.Time //* lets this be nil 
+}
+
+type Todos []Todo
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		return errors.New("invalid index")
+	}
+	return nil
+}
+
+func (todos *Todos) Add(title string) { //(todos *Todos) is a pointer receiver, so we can modify the original slice
+	todo := Todo{
+		Title:     title,
+		CreatedAt: time.Now(),
+	}
+
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) delete(index int) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+
+	*todos = slices.Delete(*todos, index, index+1) // Delete can be used on slices 
+	return nil
 }
