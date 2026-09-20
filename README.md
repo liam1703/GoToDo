6fbc020b81ef66038bcf4e65afa26d5570803f44
Liam Bradbury <55163640+liam1703@users.noreply.github.com>
2026-09-20 18:50:17 +0100

    Make project runnable

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # GoToDo
 
 A CLI todo list written in Go programming language. 
+
+
+  go run . -add "buy milk"
+  go run . -list
+  go run . -toggle 0
+  go run . -del 0
+  go run . -edit "0:buy oat milk"
--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	todos := Todos{}
+	storage := NewStorage[Todos]("todos.json")
+
+	if err := storage.Load(&todos); err != nil && !errors.Is(err, os.ErrNotExist) {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	cmdFlags := NewCmdFlags()
+	if err := cmdFlags.Execute(&todos); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if err := storage.Save(todos); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
